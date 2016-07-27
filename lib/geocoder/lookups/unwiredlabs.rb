c25ae4f30df47708250b5ca66efac238d1ed671a
liang wen <liangwen@liangdeMBP.fritz.box>
2016-07-27 12:37:33 +1000

    hehe

--- lib/geocoder/lookups/unwiredlabs.rb
+++ lib/geocoder/lookups/unwiredlabs.rb
@@ -17,29 +17,14 @@ module Geocoder::Lookup
     end
 
     def query_url(query)
-      "#{protocol}://ap1.unwiredlabs.com/v2/address.php?type=geocoding&token=#{configuration.api_key}&format=json&q=#{url_query_string(query)}"
-    end
-
-    def what_does_url_query_string_do(query)
-      url_query_string(query)
+      "#{protocol}://ap1.unwiredlabs.com/v2/address.php?type=geocoding&token=#{configuration.api_key}&format=json&q=#{query.sanitized_text}"
     end
 
     private
 
     def results(query)
       return [] unless doc = fetch_data(query)
-
-      if doc['error']
-        if doc['error'] == 'invalid API key'
-          raise_error(Geocoder::InvalidApiKey) || Geocoder.log(:warn, 'Invalid unwiredlabs API key.')
-        else
-          Geocoder.log(:warn, "Unwiredlabs API error: #{doc['error']}.")
-        end
-
-        return []
-      else
-        return [ doc ]
-      end
+      return doc
     end
   end
 end
